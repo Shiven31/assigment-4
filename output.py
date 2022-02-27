6407418d9f86df0b5f847e0b5b80fa38cdafd6cb
Shiven31 <96771819+Shiven31@users.noreply.github.com>
2022-02-27 20:27:46 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/dell/Desktop/input.py ===================
+Question1
+ the tower of Hanoi with three disks.
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Question2
+Enter the value of n: 6
+
+Using recursions:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+ Using loops:
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Question3
+Enter the first integer: 4
+Enter the second integer: 3
+Remainder is:  1
+Quotient is:  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+Question4
+Object has been created
+The name of the student it Shiven and SID is 21104017.
+Object has been destroyed
+
+Question5
+part a
+The updated salary of the employee Mehak is 70000
+part b
+Employee Viren record deleted
+
+Question6
+Enter the first word: hello
+
+Enter a new meaningful word with the exact same letters : olleh
+
+Does the word makes sense?(y or n)
+n
+The word doesn't have a meaning,i.e. friendship is fake..
+
